8037f103b507ba4bde126d6c53f7355d9cf33415
Gopi-rithi <44063388+Gopi-rithi@users.noreply.github.com>
2018-10-24 23:58:54 +0530

    Update test1.tf

--- test1.tf
+++ test1.tf
@@ -1,10 +1,10 @@
 provider "aws" {
-access_key = "AKIAJHENIGDKFTRM2X4A"
-secret_key = "asdjljljasd;lfpoowpotrtt"
+access_key = "AKIAIMJWQHS5ASYK7KBQ"
+secret_key = "cvWdysy8zFqd/Wy1hg4fAZRP8LX6g3vYobXE1c1q"
 region = "us-east-1"
 }
 
-resource "aws_instance" "example" {
-ami = "ami-6871a115"
+resource "aws_instance" "windows" {
+ami = "ami-0922553b7b0369273"
 instance_type = "t2.micro"
 }
